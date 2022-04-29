56a3e93a4ad60f37c711469954494dcd20330807
ciao409 <yuciao276@gmail.com>
2022-04-29 16:11:10 +0800

    ciao409

--- src/com/fju/order/Order.java
+++ src/com/fju/order/Order.java
@@ -2,10 +2,15 @@ package com.fju.order;
 
 public class Order {
     int amount;
+    int total
     Delivery delivery;
 
     public Order(int amount,Delivery delivery) {
         this.amount = amount;
         this.delivery = delivery;
+    }
+    public int total() {
+        total=amount + delivery.price;
+        return total;
     }
 }
